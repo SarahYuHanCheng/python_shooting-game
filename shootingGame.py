4e5e86e99f41204ce981a9001cd3115200e21b8c
SarahYuHanCheng <bigbigtreeful@gmail.com>
2018-06-11 20:24:08 +0800

    insert customize event with set_timer

--- shootingGame.py
+++ shootingGame.py
@@ -1,7 +1,21 @@
-import sys, pygame
-
+import sys, pygame, random
 from pygame.locals import *
 
+class Enemy(pygame.sprite.Sprite):
+	"""docstring for Enemy"""
+	def __init__(self):
+		super(Enemy, self).__init__()
+		self.image = pygame.image.load('missile.png').convert()
+		self.image.set_colorkey((255,255,255), RLEACCEL)
+		self.rect = self.image.get_rect(
+			center=(random.randint(820,900), random.randint(0,600))) 
+		self.speed = random.randint(5,20)
+
+	def update(self):
+			self.rect.move_ip(-self.speed,0)
+			if self.rect.right<0:
+				self.kill() # kill() is built-in sprite class
+				
 class Player(pygame.sprite.Sprite):
 	"""docstring for Player"""
 	def __init__(self):
@@ -38,15 +52,28 @@ screen = pygame.display.set_mode((800,600))# (800,600)is tuple
 # screen is a surface
 
 player = Player()
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+for entity in all_sprites:
+	screen.blit(entity.surf, entity.rect)
+ADDENEMY = pygame.USEREVENT+1 # 自定義事件需要比USEREVENT大
+pygame.time.set_timer(ADDENEMY, 250)
 
 running = True
 while running:
 	for event in pygame.event.get():
-		if event.type == QUIT: 
+		if event.type == KEYDOWN:
+			if event.key == K_ESCAPE:
+				running = False
+		elif event.type == QUIT: 
 			running = False
+		elif event.type == ADDENEMY:
+			new_enemy = Enemy()
+			enemies.add(new_enemy)
+			all_sprites.add(new_enemy)
+
 		pressed_keys = pygame.key.get_pressed()
-		if pressed_keys[K_ESCAPE]: # K_ESCAPE is in pygame.locals
-			running = False
 		player.update(pressed_keys)
 		screen.blit(player.surf,player.rect)# (400,300)no move
 		pygame.display.flip()
