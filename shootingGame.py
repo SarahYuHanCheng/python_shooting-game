d943dc92d3665e9b18865f054fc95ce6d40ec681
SarahYuHanCheng <bigbigtreeful@gmail.com>
2018-06-11 15:47:41 +0800

    detect key pressd & close screen

--- shootingGame.py
+++ shootingGame.py
@@ -1,5 +1,17 @@
-import pygame
+import sys, pygame
 
 from pygame.locals import *
 
 pygame.init()
+
+screen = pygame.display.set_mode((800,600))# (800,600)is tuple
+
+running = True
+while running:
+	for event in pygame.event.get():
+		if event.type == QUIT: # KEYDOWN is in pygame.locals
+			running = False
+
+	keys = pygame.key.get_pressed()
+	if keys[K_ESCAPE]:
+		running = False
